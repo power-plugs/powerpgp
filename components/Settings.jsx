20d6318c82e9cd6afb10cb14efa4ffdc9d15357a
Lil Sizzurp <lilsizzurp@pm.me>
2020-08-01 14:35:48 +0200

    Add link to footer of settings

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -15,6 +15,11 @@ module.exports = class Settings extends React.Component {
                 <TextArea autoCorrect="off" value={this.settings.get("pubkey")}  rows={6} onChange={val => this.settings.set("pubkey" , val)}>Public Key</TextArea>
                 <TextArea autoCorrect="off" value={this.settings.get("privkey")} rows={6} onChange={val => this.settings.set("privkey", val)} note={<><b style={{ color: 'rgb(240, 71, 71)' }}>WARNING:</b> Sharing this key will allow anyone to sign any message in your name.</>}>Private Key</TextArea>
                 <TextInput type="password" autoCorrect="off" value={this.settings.get("passphrase")} onChange={val => this.settings.set("passphrase", val)} readonly={true}>Passphrase</TextInput>
+                <Card style={{"padding":"18px"}}>
+                    <FormText>
+                        Feel free to check out some of my other plugins on <a href="https://github.com/LilSizzurp?tab=repositories&q=power" target="_BLANK">GitHub</a>!
+                    </FormText>
+                </Card>
             </div>
         );
     }
