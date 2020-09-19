fc6f322c88bd54349c228e379f961de725550d0a
Lil Sizzurp <lilsizzurp@pm.me>
2020-09-19 12:25:49 +0200

    Remove link to github.

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -2,7 +2,6 @@ const { React, getModuleByDisplayName } = require('powercord/webpack');
 const { TextInput } = require('powercord/components/settings');
 const { Card, AsyncComponent } = require('powercord/components');
 const TextArea = require("./TextArea")
-const FormText = AsyncComponent.from(getModuleByDisplayName('FormText'));
 
 module.exports = class Settings extends React.Component {
     constructor(props) {
@@ -17,11 +16,6 @@ module.exports = class Settings extends React.Component {
                 <TextArea autoCorrect="off" value={this.settings.get("pubkey")}  rows={6} onChange={val => this.settings.set("pubkey" , val)}>Public Key</TextArea>
                 <TextArea autoCorrect="off" value={this.settings.get("privkey")} rows={6} onChange={val => this.settings.set("privkey", val)} note={<><b style={{ color: 'rgb(240, 71, 71)' }}>WARNING:</b> Sharing this key will allow anyone to sign any message in your name.</>}>Private Key</TextArea>
                 <TextInput type="password" autoCorrect="off" value={this.settings.get("passphrase")} onChange={val => this.settings.set("passphrase", val)} readonly={true}>Passphrase</TextInput>
-                <Card style={{"padding":"18px"}}>
-                    <FormText>
-                        Feel free to check out some of my other plugins on <a href="https://github.com/power-plugs?tab=repositories" target="_BLANK">GitHub</a>!
-                    </FormText>
-                </Card>
             </div>
         );
     }
